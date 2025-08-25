23f861ceab28c67ec6a683beeaeafc14113a092c
s-yamauchi07 <hand1nhand306@gmail.com>
2025-08-25 13:17:08 +0900

    トップページの編集

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Movimo!</Text>
+    <View className='flex-1 bg-white items-center justify-center'>
+      <Text className='bg-red-500 text-white'>Welcome to Movimo!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
